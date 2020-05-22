0d304f3f9acf4b0c081d0137839787901ae0dcd3
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-22 16:48:11 +0300

    Static Site Generation (SSG)

---
+++ ssg-example/test-migration.js
@@ -0,0 +1,13 @@
+const sqlite = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+(async () => {
+  const db = await sqlite.open({
+    filename: "./microphones.sqlite",
+    driver: sqlite3.Database,
+  });
+
+  await db.migrate({ force: true });
+  const microphones = await db.all("select * from microphone");
+  console.log(JSON.stringify(microphones, null, 2));
+})();
