1fcab1874f4203e4092cdbf22f73c83163f3bb1c
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-23 12:46:42 +0300

    Data Fetching with getServerSideProps

---
+++ ssg-example_ssp/src/pages/index.tsx
@@ -0,0 +1,31 @@
+import { GetServerSideProps } from "next";
+import Link from 'next/link';
+import { Microphone } from "../../model/Microphone";
+import { openDB } from "../openDB";
+
+export interface IndexProps {
+  microphones: Microphone[];
+}
+
+export default function Index({ microphones } : IndexProps) {
+  return (
+    <div>
+      <Link href='/people'>
+        <a>People</a>
+      </Link>
+      <pre>{JSON.stringify(microphones, null, 2)}</pre>
+    </div>
+
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<IndexProps> = async ctx => {
+  const db = await openDB();
+  const microphones = await db.all<Microphone[]>('select * from microphone')
+
+  await new Promise(acc => {
+    setTimeout(acc, 3000);
+  });
+
+  return { props: { microphones } };
+}
